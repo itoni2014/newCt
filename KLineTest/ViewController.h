c53bb768c164c2768d6e85525bc2f0fa5ac9f142
ias sec <ias_sec@kabushikigaishaias-no-imac.local>
2016-01-25 10:06:14 +0900

    Initial Commit

---
+++ KLineTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KLineTest
+//
+//  Created by ias_sec on 2016/01/25.
+//  Copyright © 2016年 TnsStudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ KLineTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  KLineTest
+//
+//  Created by ias_sec on 2016/01/25.
+//  Copyright © 2016年 TnsStudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
